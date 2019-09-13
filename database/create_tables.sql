a2a625f2bb8634ff99c8cfe5c67bb97826cf1c7f
Ian Wu <iwle@u.nus.edu>
2019-09-14 00:57:58 +0800

    Add more CREATE statements Add CREATE TABLE for ProjectFeedback, UserFeedback, ProjectUpdates, and ProjectTiers.

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -66,3 +66,41 @@ CREATE TABLE PaymentInfo (
       credit_card_expiry_year
       CHECK (credit_card_expiry_year > (extract(epoch from date_trunc('year', CURRENT_DATE))::int))
 );
+
+CREATE TABLE ProjectFeedback (
+    feedback_id serial PRIMARY KEY,
+    project_id int REFERENCES Projects(project_id) ON DELETE CASCADE,
+    comment_text text,
+    rating_number int,
+
+    CONSTRAINT
+        feedback_not_null
+        CHECK (comment_text IS NOT NULL OR rating_number IS NOT NULL)
+);
+
+CREATE TABLE UserFeedback(
+    user_id int REFERENCES USERS(user_id),
+    feedback_id int REFERENCES ProjectFeedback(feedback_id) ON DELETE CASCADE
+);
+
+CREATE TABLE ProjectUpdates (
+    update_id serial,
+    project_id int REFERENCES Projects(project_id) ON DELETE CASCADE,
+    update_description text,
+    update_date date,
+
+    CONSTRAINT
+      project_update_constraint
+      PRIMARY KEY(update_id, project_id)
+);
+
+CREATE TABLE ProjectTiers (
+    tier_id serial,
+    project_id int REFERENCES Projects(project_id) ON DELETE CASCADE,
+    threshold_amount numeric(20,2),
+    reward_description text,
+    PRIMARY KEY(project_id, tier_id),
+    CONSTRAINT
+      project_tier_constraint
+      UNIQUE(project_id, threshold_amount)
+);
